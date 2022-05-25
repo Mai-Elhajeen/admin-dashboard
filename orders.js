af88ca0c9574e3ec2a12d2ec404f291b0b447ab6
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-05-26 00:29:55 +0300

    adding js code and filling orders table using js

---
+++ orders.js
@@ -0,0 +1,32 @@
+const orders = [
+    {
+        productName: 'Product 1',
+        productNumber: '25878',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Product 2',
+        productNumber: '5864',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Product 3',
+        productNumber: '18953',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Product 4',
+        productNumber: '59712',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Product 5',
+        productNumber: '94687',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    }
+]
